8da7cb891375097faa33cc8cc3505787f29b8b1c
Carlos Tojal <carlos.tojal@hotmail.com>
2022-12-10 19:28:55 +0000

    fix mean time computing

--- ewok_simulation/benchmarking/compute_mean_time.py
+++ ewok_simulation/benchmarking/compute_mean_time.py
@@ -7,8 +7,8 @@ import numpy as np
 mapping_data = np.loadtxt('mapping_time.txt')
 opt_data = np.loadtxt('optimization_time.txt')
 
-print 'mapping time: computing pointcloud, moving volume, inserting measurements [ms]'
-print mapping_data.mean(axis=0)/1e6
+print('mapping time: computing pointcloud, moving volume, inserting measurements [ms]')
+print(mapping_data.mean(axis=0)/1e6)
 
-print 'sdf computation, optimization time [ms]'
-print opt_data.mean(axis=0)/1e6
+print('sdf computation, optimization time [ms]')
+print(opt_data.mean(axis=0)/1e6)
